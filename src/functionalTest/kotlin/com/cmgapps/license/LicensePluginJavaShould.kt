0c50cdf4f53241ffd2149c52e03dc8e17f3f433c
Christian Grach <christian.grach@cmgapps.com>
2025-01-04 12:40:07 +0100

    add gradle 8.12 to tests

--- src/functionalTest/kotlin/com/cmgapps/license/LicensePluginJavaShould.kt
+++ src/functionalTest/kotlin/com/cmgapps/license/LicensePluginJavaShould.kt
@@ -66,7 +66,7 @@ class LicensePluginJavaShould {
     @ValueSource(
         strings = [
             "7.2", "7.3", "7.4", "7.5", "7.6", "8.0", "8.1", "8.2", "8.3", "8.4",
-            "8.5", "8.6", "8.7", "8.8", "8.9", "8.10", "8.11",
+            "8.5", "8.6", "8.7", "8.8", "8.9", "8.10", "8.11", "8.12",
         ],
     )
     fun `apply Licenses plugin to various Gradle versions`(version: String) {
