3f398ca475b3643d2eec5540b9270b3879ec5543
Christian Grach <christian.grach@cmgapps.com>
2026-02-07 17:14:43 +0100

    use fixtures for xml validator test

--- src/functionalTest/kotlin/com/cmgapps/license/XmlValidationShould.kt
+++ src/functionalTest/kotlin/com/cmgapps/license/XmlValidationShould.kt
@@ -6,62 +6,35 @@
 
 package com.cmgapps.license
 
-import com.cmgapps.license.util.plus
-import org.gradle.testkit.runner.GradleRunner
+import com.cmgapps.license.util.createBuildRunner
+import com.cmgapps.license.util.fixturesDir
 import org.hamcrest.MatcherAssert.assertThat
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.io.TempDir
 import org.xmlunit.builder.Input
 import org.xmlunit.matchers.ValidationMatcher.valid
-import java.net.URL
-import java.nio.file.Files
-import java.nio.file.Path
-import java.nio.file.Paths
+import java.io.File
+import kotlin.io.path.Path
 
 class XmlValidationShould {
-    @TempDir
-    lateinit var testProjectDir: Path
-
     @Test
     fun validateSchema() {
-        val reportFolder = "$testProjectDir/build/reports/licenses/licenseReport"
-        val mavenRepoUrl = javaClass.getResource("/maven")!!.toString()
-        Files.createFile(Paths.get(testProjectDir.toString(), "build.gradle")).toFile() +
-            """
-            plugins {
-               id("java")
-               id("com.cmgapps.licenses")
-            }
-
-            repositories {
-                maven {
-                    url '$mavenRepoUrl'
-                }
-            }
-            
-            licenses {
-              reports {
-                  html.enabled.set(false)
-                  xml.enabled.set(true)
-              }
-            }
-            
-            dependencies {
-              implementation 'group:multilicenses:1.0.0'
-              implementation 'group:name:1.0.0'
-            }
-            """.trimIndent()
-
-        GradleRunner
-            .create()
-            .withProjectDir(testProjectDir.toFile())
-            .withArguments(":licenseReport")
-            .withPluginClasspath()
-            .build()
+        val fixtureDir = File(fixturesDir, "xml-validator")
+        createBuildRunner(fixtureDir).build()
 
         assertThat(
-            Input.fromFile("$reportFolder/licenses.xml"),
-            valid(Input.fromURL(URL("https://www.cmgapps.com/xsd/licenses.xsd"))),
+            Input.fromFile(
+                File(fixtureDir, "build/reports/licenses/licenseReport/licenses.xml"),
+            ),
+            valid(
+                Input.fromURI(
+                    Path(
+                        fixturesDir.parentFile.parent,
+                        "main",
+                        "xsd",
+                        "licenses.xsd",
+                    ).toUri(),
+                ),
+            ),
         )
     }
 }
